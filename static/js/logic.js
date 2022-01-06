0aa0b87812a5f1518a66480b2483737a482332db
Mark <melendez5113@gmail.com>
2022-01-05 21:12:15 -0500

    Mapping GeoJSON Points

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,25 +2,37 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([40.7, -94.5], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [37.6213, -122.3790],
-  [30.1975, -97.6664],
-  [43.6777, -79.6248],
-  [40.2859, -74.3486],
-  [40.6413, -73.7781]
-];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "blue",
-  weight: "4",
-  dashArray: '5, 5',
-  dashOffset: '0',
-  opacity: '0.5'
-}).addTo(map);
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h1> <hr> <h3>Airport name: " + feature.properties.name + "</h3>")
+    }
+
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
