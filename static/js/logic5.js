69a8ecaf2e58ffd62f60472ae7623e33c1ea5c1e
Mark <melendez5113@gmail.com>
2022-01-09 16:12:26 -0500

    Earthquakes Past 7-Days

--- static/js/logic5.js
+++ static/js/logic5.js
@@ -37,14 +37,9 @@ let map = L.map('mapid', {
 // Then we add a control to the map that will allow the user to change which layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);
 
-// This function determines the radius of the earthquake marker based on its magnitude.
-// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
-
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-    // This function returns the style data for each of the earthquakes we plot on
-    // the map. We pass the magnitude of the earthquake into two separate functions
-    // to calculate the color and radius.
+    // This function returns the style data for each of the earthquakes we plot on the map. We pass the magnitude of the earthquake into two separate functionsto calculate the color and radius.
     function styleInfo(feature) {
         return {
         opacity: 1,
@@ -77,6 +72,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return "#98ee00";
     };
 
+    // This function determines the radius of the earthquake marker based on its magnitude. Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
     function getRadius(magnitude) {
         if (magnitude === 0) {
           return 1;
